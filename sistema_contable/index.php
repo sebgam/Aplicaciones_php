1402816b634e3468f46cec56b595250159f77595
sebas <sebastiangamboa11@gmail.com>
2017-03-21 13:24:55 -0500

    htaccess

--- sistema_contable/index.php
+++ sistema_contable/index.php
@@ -1,13 +1,10 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
 
-</body>
-</html>
+//------definir rutas--------------
+define("RUTA_BASE",dirname(realpath(__FILE__))."/");
+
+include "libreria/core.php";
+
+
+
+?>
---
+++ sistema_contable/libreria/core.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 21/03/2017
+ * Time: 1:23 PM
+ */
